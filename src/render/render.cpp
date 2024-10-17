38830ad30666c7b7e2016980ce86c2ab6e1945e8
Paul <98259426+Choooca@users.noreply.github.com>
2024-10-18 00:17:57 +0200

    Refactoring

---
+++ src/render/render.cpp
@@ -0,0 +1,79 @@
+#include "render.hpp"
+
+Render::Render() {};
+
+void Render::RenderInit()
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	window = SDL_CreateWindow("Minecraft", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+	if (!window)
+	{
+		std::cout << "Failed To Create Window : " << SDL_GetError() << std::endl;
+		exit(-1);
+	}
+
+	context = SDL_GL_CreateContext(window);
+	if (!context)
+	{
+		std::cout << "Failed to create Context : " << SDL_GetError() << std::endl;
+		exit(-1);
+	}
+
+	SDL_GL_MakeCurrent(window, context);
+
+	if (!gladLoadGLLoader(SDL_GL_GetProcAddress))
+	{
+		std::cout << "Failed to initialized glad : " << glGetError() << std::endl;
+		exit(-1);
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	shader = new Shader("C:/LearnCpp/Minecraft/shader/default.vert", "C:/LearnCpp/Minecraft/shader/default.frag");
+}
+
+void Render::RenderInitTriangles()
+{
+	float vertices[] = {
+		0.0f, .5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f};
+
+	glGenVertexArrays(1, triangleRenderData->VAO);
+	glGenBuffers(1, triangleRenderData->VBO);
+
+	glBindVertexArray(*triangleRenderData->VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, *triangleRenderData->VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+void Render::RenderUpdate()
+{
+	glClearColor(.1f, .1f, .1f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBindVertexArray(*triangleRenderData->VAO);
+
+	shader->use();
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	SDL_GL_SwapWindow(window);
+}
+
+void Render::DestroyRender()
+{
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
---
+++ src/render/render.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <glad/glad.h>
+#include "../shader/shader.hpp"
+
+class Render
+{
+public:
+	typedef struct
+	{
+		unsigned int *VAO;
+		unsigned int *VBO;
+		unsigned int *EBO;
+	} RenderData;
+
+	Render();
+
+	void RenderInit();
+	void RenderInitTriangles();
+	void RenderUpdate();
+	Shader *GetShader();
+	void DestroyRender();
+
+	RenderData *triangleRenderData;
+
+private:
+	Shader *shader;
+	SDL_Window *window;
+	SDL_GLContext context;
+};
